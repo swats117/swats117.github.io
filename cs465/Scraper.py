fe7fe8177a8b0938b774328ec8f8b0ad7d5f6527
Lavanya Piramanayagam <lavanyap@wirelessprv-10-195-84-26.near.illinois.edu>
2018-04-21 13:38:15 -0500

    Changes

--- cs465/Scraper.py
+++ cs465/Scraper.py
@@ -4,7 +4,7 @@ from urllib.request import urlopen
 
 from bs4 import BeautifulSoup
 #insert URL of google scholar page of a specific author 
-quote_page = "https://scholar.google.com/citations?user=_EPCKe0AAAAJ&hl=en"
+quote_page = "https://scholar.google.com/citations?user=ZVQV4CAAAAAJ&hl=en"
 page = urlopen(quote_page)
 soup = BeautifulSoup(page, "html.parser")
 
@@ -28,7 +28,7 @@ divs = soup.find_all("div", attrs={"class": "gs_gray"})
 ###############################################################PAPER###########################################################
 for pap in paper:
 	#print (pap.text.strip())
-	with open('Josh_Cogan_paper.txt','a') as the_file:
+	with open('Kavi_Mahesh_paper.txt','a') as the_file:
 		the_file.write(pap.text.strip()+"\n")
 
 
@@ -36,23 +36,23 @@ for pap in paper:
 
 for cit in citations:
 	#print (cit.text.strip())
-	with open('Josh_Cogan_citations.txt','a') as the_file:
+	with open('Kavi_Mahesh_citations.txt','a') as the_file:
 		the_file.write(cit.text.strip()+"\n")
 ###############################################################TOTAL STATS#######################################################
 for tot in total:
 	#print (tot.text.strip())
-	with open('Josh_Cogan_total.txt','a') as the_file:
+	with open('Kavi_Mahesh_total.txt','a') as the_file:
 		the_file.write(tot.text.strip()+"\n")
 
 ###############################################################COAUTHOR########################################################
 for div in divs:
     #print (div.text.strip())
-    with open('Josh_Cogan.txt', 'a') as the_file:
+    with open('Kavi_Mahesh.txt', 'a') as the_file:
     	the_file.write(div.text.strip()+"\n")
 
-fp1 = open('Josh_Cogan_author.txt', 'w+')
+fp1 = open('Kavi_Mahesh_author.txt', 'w+')
 
-with open('Josh_Cogan.txt') as fp:
+with open('Kavi_Mahesh.txt') as fp:
 	line1 = fp.readline()
 	line2 = fp.readline()
 	while line1 and line2:
